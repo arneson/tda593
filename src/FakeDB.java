3bd9919ecec6578c2dad6e6ea50a30ec8235710b
scanniza <simon@cymonit.se>
2015-12-09 11:29:18 +0100

    Added init to FakeDb

--- src/FakeDB.java
+++ src/FakeDB.java
@@ -1,4 +1,22 @@
+import java.util.ArrayList;
+
+import HotelManagementClassDiagram.Booking;
+import HotelManagementClassDiagram.Customer;
+import HotelManagementClassDiagram.Discount;
+import HotelManagementClassDiagram.Employee;
+import HotelManagementClassDiagram.Room;
 
 public class FakeDB {
-
+	public static ArrayList<Booking> bookings;
+	public static ArrayList<Room> rooms;
+	public static ArrayList<Customer> customers;
+	public static ArrayList<Employee> employees;
+	public static ArrayList<Discount> discounts;
+	public static void init(){
+		bookings = new ArrayList<Booking>();
+		rooms = new ArrayList<Room>();
+		customers = new ArrayList<Customer>();
+		employees = new ArrayList<Employee>();
+		discounts = new ArrayList<Discount>();
+	}
 }
